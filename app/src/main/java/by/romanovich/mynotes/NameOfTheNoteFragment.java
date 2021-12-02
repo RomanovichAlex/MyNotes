3b0721c880fd6bdd36dbcb54aad2f90e30cae883
RomanovichAlex <romanowitch.alex@yandex.ru>
2021-12-02 15:32:07 +0300

    Избавился от портретной ориентации

--- app/src/main/java/by/romanovich/mynotes/NameOfTheNoteFragment.java
+++ app/src/main/java/by/romanovich/mynotes/NameOfTheNoteFragment.java
@@ -1,6 +1,5 @@
 package by.romanovich.mynotes;
 
-import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,19 +11,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class NameOfTheNoteFragment extends Fragment {
-
-    private static final String CURRENT_NOTE = "CurrentNote";
-    // Текущая позиция (выбранная заметка)
-    private int currentPosition = 0;
 
     // При создании фрагмента Создаём макет экрана и отображения названиия заметок
     @Override
@@ -39,16 +32,10 @@ public class NameOfTheNoteFragment extends Fragment {
     @Override
     public void onViewCreated (@NonNull View view, @NonNull Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
-        if (savedInstanceState != null) {
-            currentPosition = savedInstanceState.getInt(CURRENT_NOTE, 0);
-        }
-        initList(view);
 
-    // отображения открытого ранее списка в ландшафтной ориентации
-        if (isLandscape()) {
-        showLandDescription(currentPosition);
-    }
+        initList(view);
 }
+
     //Передаем layot, создаём список заметок на экране из массива в ресурсах
     private void initList(View view) {
         LinearLayout layoutView = (LinearLayout) view;
@@ -62,17 +49,9 @@ public class NameOfTheNoteFragment extends Fragment {
             layoutView.addView(tv);
             final int position=i;
             tv.setOnClickListener(v -> {
-                currentPosition=position;
-                showDescription(position);
+                int currentPosition = position;
+                showPortDescription(position);
             });
-        }
-    }
-
-    private void showDescription(int index) {
-        if (isLandscape()) {
-            showLandDescription(index);
-        } else {
-            showPortDescription(index);
         }
     }
 
@@ -86,28 +65,5 @@ public class NameOfTheNoteFragment extends Fragment {
         fragmentTransaction.addToBackStack("");
         fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         fragmentTransaction.commit();
-    }
-
-    // Показываем описание в ландшафтной ориентации
-    private void showLandDescription(int index) {
-        // Создаём новый фрагмент с текущей позицией для вывода герба
-        NoteFragment detail = NoteFragment.newInstance(index);
-        // Выполняем транзакцию по замене фрагмента
-        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.NoteFragment, detail);  // замена фрагмента
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        fragmentTransaction.commit();
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putInt(CURRENT_NOTE, currentPosition);
-        super.onSaveInstanceState(outState);
-    }
-
-    private boolean isLandscape() {
-        return getResources().getConfiguration().orientation
-                == Configuration.ORIENTATION_LANDSCAPE;
     }
 }
